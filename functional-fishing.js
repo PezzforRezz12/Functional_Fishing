06e554f654ad280e33ee2a5686239604cc0ad275
John Marsland <mmmjohnnnyww@gmail.com>
2020-02-08 14:08:10 -0500

    https://github.com/PezzforRezz12/Functional_Fishing.git

---
+++ functional-fishing.js
@@ -0,0 +1,49 @@
+console.log(`Welcome To Functional Fishing`);
+
+console.log(`INSTRUCTIONS`);
+
+console.log(`In this game, You will catch, cook and eat a fish. You will also search for wood and bait. `)
+
+console.log('hello')
+
+
+
+
+
+function fire () {
+console.log = 'You Need Wood To Start The Fire'
+console.log = 'The player has at least 1 piece of wood'
+}
+
+function fish () {
+ console.log = (`The player has at least 1 piece of bait`) 
+ console.log = (`The fire has been stopped`)  
+}
+
+
+function bait () { 
+    console.log = (`The fire has been stopped`)
+    
+}
+
+function wood () {
+    console.log = (`The fire has been stopped`)
+}
+
+function eat () {
+    console.log = 'you must start a fire';
+}
+
+
+function inventory () {
+console.log("The player has 1 bucket of water");
+console.log("The player has at least 1 piece of bait");
+console.log("The player has at least 1 piece of wood");
+}
+
+
+
+
+function help () {
+    return (`Welcome to Functional Fishing.
+  The text-based console fishing game.`)}
