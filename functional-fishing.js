e27acd004cc61b2abf83e789b068fd88f9775d5c
John Marsland <mmmjohnnnyww@gmail.com>
2020-02-10 12:31:42 -0500

    Completed Functional Fishing

--- functional-fishing.js
+++ functional-fishing.js
@@ -1,49 +1,88 @@
-console.log(`Welcome To Functional Fishing`);
+    console.log(`Welcome To Functional Fishing`);
 
-console.log(`INSTRUCTIONS`);
+    console.log(`INSTRUCTIONS`);
 
-console.log(`In this game, You will catch, cook and eat a fish. You will also search for wood and bait. `)
+    console.log(`In this game, You will catch, cook and eat a fish. You will also search for wood and bait. `)
 
-console.log('hello')
-
-
+    console.log('Instructions ')
 
 
-
-function fire () {
-console.log = 'You Need Wood To Start The Fire'
-console.log = 'The player has at least 1 piece of wood'
-}
-
-function fish () {
- console.log = (`The player has at least 1 piece of bait`) 
- console.log = (`The fire has been stopped`)  
-}
+    var baitCounter = 0
+    var woodCounter = 0
+    var hasFire = false
+    var fishCounter = 0
 
 
-function bait () { 
-    console.log = (`The fire has been stopped`)
+    function fire () {
+
+    if (woodCounter > 0) {
+
+        if (hasFire === true){
+            hasFire = false
+            console.log(`You have stop the fire`)       
+        }
+        if (hasFire === false){
+            hasFire = true;
+            woodCounter = woodCounter -1;
+            console.log(`You have started a fire`)
+        }
+    } else{
+            console.log ('You Need Wood To Start The Fire')
+        }
+    }
+
+
+    function fish () {
+    if (baitCounter > 0 && hasFire === false){
+    baitCounter = baitCounter -1
+    fishCounter = fishCounter +1
+        console.log(`you have caught 1 fish`)
+    } else if (baitCounter === 0){
+    console.log(`You have no bait`)
+    } else {
+        console.log(`You must put out the fire`)
+    }
+    }
+
+
+    function bait () { 
+    baitCounter = baitCounter + 1
+        console.log(`You have found 1 piece of bait`)
+
+        
+        
+    }
+
+    function wood () {
+        woodCounter = woodCounter + 1;
+        console.log(`You have found 1 piece of wood`)
+    }
+
+    function eat () {
+        if (hasFire === false){
+            console.log(`You must start the fire`)
+        }
+        else if (hasFire === true && fishCounter > 0)
+        console.log(`You have eaten 1 fish `)
+        fishCounter = fishCounter - 1 
+    }
+
+
+    function inventory () {
+    console.log(`You Have` + woodCounter + `pieces of wood`)
+    console.log(`You Have` + baitCounter + `pieces of bait`)
+    console.log(`You Have` + fishCounter + `pieces of fish`)
+    if (hasFire === true){
+        console.log(`the fire is burning`)
+    }
+    else{
+        console.log(`the fire is out`)
+    }
+    }
+
+
+
+    function help () {
+        return (`Welcome to Functional Fishing.
+    The text-based console fishing game.`)}
     
-}
-
-function wood () {
-    console.log = (`The fire has been stopped`)
-}
-
-function eat () {
-    console.log = 'you must start a fire';
-}
-
-
-function inventory () {
-console.log("The player has 1 bucket of water");
-console.log("The player has at least 1 piece of bait");
-console.log("The player has at least 1 piece of wood");
-}
-
-
-
-
-function help () {
-    return (`Welcome to Functional Fishing.
-  The text-based console fishing game.`)}
